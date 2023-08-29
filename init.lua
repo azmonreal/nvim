ffee97af9b3de0036b94c4e4e2f4bf7746c21dc2
azmonreal <azmonreal@gmail.com>
2023-08-29 14:13:06 -0600

    .

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ local modules = {
 
 for _, v in pairs(modules) do
 	local status, error = pcall(require, "azmon." .. v)
-if not status then
-	vim.notify(error)
-end
+	if not status then
+		vim.notify(error)
+	end
 end
