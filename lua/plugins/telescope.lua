42c61f0d10c573868cfaec00802b4084cab971a2
azmonreal <azmonreal@gmail.com>
2024-02-28 10:42:09 -0600

    fix telescope lazy loading issues

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,34 +1,32 @@
-local function MappingsToKeys()
-	local keys = {}
+local function set_custom_maps(customMaps)
+	for _, map_group in pairs(customMaps) do
+		if map_group.leader == nil then map_group.leader = "" end
+		for _, keymap in pairs(map_group[1]) do
+			local mode, rh, lf, opts = unpack(keymap)
 
-	local leader = "f"
-
-	local mappings = {
-		{ { "n" }, "<leader>", function() require("telescope.builtin").builtin() end,                   { desc = "Open telescope picker list" } },
-		{ { "n" }, "g",        function() require("telescope.builtin").live_grep() end,                 { desc = "Open telescope live grep" } },
-		{ { "n" }, "/",        function() require("telescope.builtin").current_buffer_fuzzy_find() end, { desc = "Open telescope current buffer fuzzy find" } },
-		{ { "n" }, "b",        function() require("telescope.builtin").buffers() end,                   { desc = "Open telescope buffer list" } },
-		{ { "n" }, "h",        function() require("telescope.builtin").help_tags() end,                 { desc = "Open telescope help search" } },
-		{ { "n" }, "k",        function() require("telescope.builtin").keymaps() end,                   { desc = "Open telescope keymap list" } },
-		{ { "n" }, "r",        function() require("telescope.builtin").registers() end,                 { desc = "Open telescope register list" } },
-		{ { "n" }, "j",        function() require("telescope.builtin").jumplist() end,                  { desc = "Open telescope jumplist" } },
-		{ { "n" }, "f",        function() require("telescope.builtin").find_files() end,                { desc = "Open telescope file list" } },
-		{ { "n" }, "r",        function() require("telescope.builtin").oldfiles() end,                  { desc = "Open telescope old files" } },
-		{ { "n" }, "p",        function() require("telescope").extensions.projects.projects() end,      { desc = "Open telescope projects" } },
-	}
-
-	for i, v in ipairs(mappings) do
-		local temp = {}
-		temp[1] = "<leader>" .. leader .. v[2]
-		temp[2] = v[3]
-		v[4].mode = v[1]
-		temp[3] = v[4]
-		keys[i] = temp
+			vim.keymap.set(mode, map_group.leader .. rh, lf, opts)
+		end
 	end
-
-	return keys
 end
 
+local mappings = {
+	{
+		leader = "<leader>f",
+		{
+			{ { "n" }, "<leader>", function() require("telescope.builtin").builtin() end,                   { desc = "Open telescope picker list" } },
+			{ { "n" }, "g",        function() require("telescope.builtin").live_grep() end,                 { desc = "Open telescope live grep" } },
+			{ { "n" }, "/",        function() require("telescope.builtin").current_buffer_fuzzy_find() end, { desc = "Open telescope current buffer fuzzy find" } },
+			{ { "n" }, "b",        function() require("telescope.builtin").buffers() end,                   { desc = "Open telescope buffer list" } },
+			{ { "n" }, "h",        function() require("telescope.builtin").help_tags() end,                 { desc = "Open telescope help search" } },
+			{ { "n" }, "k",        function() require("telescope.builtin").keymaps() end,                   { desc = "Open telescope keymap list" } },
+			{ { "n" }, "r",        function() require("telescope.builtin").registers() end,                 { desc = "Open telescope register list" } },
+			{ { "n" }, "j",        function() require("telescope.builtin").jumplist() end,                  { desc = "Open telescope jumplist" } },
+			{ { "n" }, "f",        function() require("telescope.builtin").find_files() end,                { desc = "Open telescope file list" } },
+			{ { "n" }, "r",        function() require("telescope.builtin").oldfiles() end,                  { desc = "Open telescope old files" } },
+			{ { "n" }, "p",        function() require("telescope").extensions.projects.projects() end,      { desc = "Open telescope projects" } },
+		}
+	}
+}
 
 return {
 	"nvim-telescope/telescope.nvim",
@@ -59,6 +57,8 @@ return {
 		for _, e in pairs(telescope_extensions) do
 			require("telescope").load_extension(e)
 		end
+
+		set_custom_maps(mappings)
 	end,
 
 	opts = {
@@ -79,6 +79,4 @@ return {
 			},
 		}
 	},
-
-	keys = MappingsToKeys()
 }
