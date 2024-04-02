9cce5cbc3f0c8a903514bd87ec98bdd48c0e7b25
azmonreal <azmonreal@gmail.com>
2024-04-02 17:06:17 -0600

    feat: basic git functionality and keymaps using fugitive

---
+++ lua/plugins/git.lua
@@ -0,0 +1,57 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add          = { text = "│" },
+				change       = { text = "│" },
+				delete       = { text = "_" },
+				topdelete    = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked    = { text = "┆" },
+			},
+
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				-- Navigation
+				Utils.set_custom_maps({
+					[""] = {
+						{ "n", "]c", function()
+							if vim.wo.diff then return "]c" end
+							vim.schedule(function() gs.next_hunk() end)
+							return "<Ignore>"
+						end, { expr = true } },
+
+						{ "n", "[c", function()
+							if vim.wo.diff then return "[c" end
+							vim.schedule(function() gs.prev_hunk() end)
+							return "<Ignore>"
+						end, { expr = true } },
+						{ { "n" }, "<leader>gB", function() gs.blame_line{full=true} end, { desc = "Open telescope picker list" } },
+					}
+				})
+			end
+		}
+	},
+	{
+		"kdheepak/lazygit.nvim",
+	},
+	{
+		"tpope/vim-fugitive",
+
+		config = function()
+			Utils.set_custom_maps({
+				["<leader>g"] = {
+					{ { "n" }, "g", "<cmd>Git<CR>", { desc = "Open telescope picker list" } },
+					{ { "n" }, "s", "<cmd>Git status<CR>", { desc = "Open telescope picker list" } },
+					{ { "n" }, "d", "<cmd>Git diff<CR>", { desc = "Open telescope picker list" } },
+					{ { "n" }, "l", "<cmd>Git log<CR>", { desc = "Open telescope picker list" } },
+					{ { "n" }, "a", "<cmd>Git add<CR>", { desc = "Open telescope picker list" } },
+					{ { "n" }, "b", "<cmd>Git branch<CR>", { desc = "Open telescope picker list" } },
+					-- { { "n" }, "B", "<cmd>Git blame<CR>", { desc = "Open telescope picker list" } },
+				}
+			})
+		end
+	},
+}
