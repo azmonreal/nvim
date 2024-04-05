d6d6b5352fcda27477d0fe5fbeda51c64e8bf622
Azael Monreal <azmonreal@gmail.com>
2024-04-05 00:21:03 -0600

    feat: filetype stuff

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,1 +1,3 @@
 vim.keymap.set({"n"}, "<Esc>", "<cmd>bd<CR>", {buffer = true});
+
+vim.o.winfixbuf = true
--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,1 +1,15 @@
 vim.wo.conceallevel = 2
+vim.wo.wrap = true
+vim.wo.linebreak = true
+
+Utils.set_custom_maps({
+	[""] = {
+		{ "n", "j",  "gj" },
+		{ "n", "k",  "gk" },
+		{ "n", "gj", "j" },
+		{ "n", "gk", "k" },
+	}
+})
+
+
+vim.g.vimtex_view_method = "zathura"
