14b3e83cde1a2c3330f036eaed5590e3cf3661c9
Azael Monreal <azmonreal@gmail.com>
2024-05-23 17:39:32 -0600

    feat: luasnip improvements + latex snippets (ejmastnak's)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -76,6 +76,28 @@ return {
 			})
 		end
 
+			ls.config.setup({
+				store_selection_keys = "<C-K>",
+				enable_autosnippets = true
+			})
+
+
+			local sn = ls.snippet_node
+			local i = ls.insert_node
+
+			Utils["luasnip"] = {
+				get_visual =
+					function(args, parent)
+						if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+							return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+						else -- If LS_SELECT_RAW is empty, return a blank insert node
+							return sn(nil, i(1))
+						end
+					end
+			}
+
+			require("luasnip.loaders.from_lua").load({ paths = "./lua/snippets/" })
+		end
 	},
 
 	"sindrets/diffview.nvim",
---
+++ lua/snippets/tex/luatex.lua
@@ -0,0 +1,18 @@
+local get_visual = Utils["luasnip"].get_visual
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
+
+-- Return snippet tables
+return
+  {
+    -- tex.sprint
+    s({trig = "tpp", snippetType="autosnippet"},
+      fmta(
+      [[
+        tex.sprint(<>)
+      ]],
+        {
+          d(1, get_visual),
+        }
+      )
+    ),
+  }
