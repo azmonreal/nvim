ac34b17d52a2498583390b8805dc5fc5fa12af18
Azael Monreal <azmonreal@gmail.com>
2024-07-08 10:43:29 -0600

    refactor: fix terminology

--- lua/azmon/utils.lua
+++ lua/azmon/utils.lua
@@ -4,11 +4,11 @@ local M = {}
 M.set_custom_maps = function(customMaps)
 	for lleader, map_group in pairs(customMaps) do
 		for _, keymap in pairs(map_group) do
-			local mode, rhs, lf, opts = unpack(keymap)
-			rhs = type(rhs) == "string" and { rhs } or rhs
+			local mode, lhs, rhs, opts = unpack(keymap)
+			lhs = type(lhs) == "string" and { lhs } or lhs
 
-			for _, rh in ipairs(rhs) do
-				vim.keymap.set(mode, lleader .. rh, lf, opts)
+			for _, v in ipairs(lhs) do
+				vim.keymap.set(mode, lleader .. v, rhs, opts)
 			end
 		end
 	end
