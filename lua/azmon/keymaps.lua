acf78429b42bf2817389c1dfbc0299547713809c
Azael Monreal <azmonreal@gmail.com>
2024-07-24 13:20:56 -0600

    docs: quit keymaps description

--- lua/azmon/keymaps.lua
+++ lua/azmon/keymaps.lua
@@ -4,10 +4,10 @@ local custom_maps = {
 		{ { "n" }, "]b",                 "<cmd>bn<CR>",                      { desc = "Switch to next buffer" } },
 		{ { "n" }, "[b",                 "<cmd>bp<CR>",                      { desc = "Switch to previous buffer" } },
 
-		{ { "n" }, "Zz",                 function () vim.cmd([[w|bd]]) end,  { desc = "" } },
-		{ { "n" }, "ZZ",                 function () vim.cmd([[wa|qa]]) end, { desc = "" } },
-		{ { "n" }, "Zq",                 function () vim.cmd([[bd!]]) end,   { desc = "" } },
-		{ { "n" }, "ZQ",                 function () vim.cmd([[qa!]]) end,   { desc = "" } },
+		{ { "n" }, "Zz",                 function () vim.cmd([[w|bd]]) end,  { desc = "Save and delete current buffer" } },
+		{ { "n" }, "ZZ",                 function () vim.cmd([[wa|qa]]) end, { desc = "Save all buffers and quit" } },
+		{ { "n" }, "Zq",                 function () vim.cmd([[bd!]]) end,   { desc = "Delete current buffer without saving" } },
+		{ { "n" }, "ZQ",                 function () vim.cmd([[qa!]]) end,   { desc = "Exit all buffers without saving" } },
 	}
 }
 
