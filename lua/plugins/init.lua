62257555133136ff986b891fb6c2aca011e8d959
Azael Monreal <azmonreal@gmail.com>
2024-07-24 23:17:06 -0600

    feat: iron for repl stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -312,4 +312,72 @@ return {
 			},
 		},
 	},
+	{
+		'Vigemus/iron.nvim',
+		tag = "v3.0",
+		main = "iron.core",
+		init = function ()
+			local iron = require("iron.core")
+			iron.setup(
+				{
+					config = {
+						-- Whether a repl should be discarded or not
+						scratch_repl = true,
+						-- Your repl definitions come here
+						repl_definition = {
+							sh = {
+								-- Can be a table or a function that
+								-- returns a table (see below)
+								command = { "zsh" }
+							},
+							python = {
+								command = { "python3" }, -- or { "ipython", "--no-autoindent" }
+								format = require("iron.fts.common").bracketed_paste_python
+							}
+						},
+						-- How the repl window will be displayed
+						-- See below for more information
+						repl_open_cmd = require('iron.view').split.vertical.botright(50)
+					},
+					-- Iron doesn't set keymaps by default anymore.
+					-- You can set them here or manually add keymaps to the functions in iron.core
+					keymaps = {
+						send_motion = "<space>sc",
+						visual_send = "<space>sc",
+						send_file = "<space>sf",
+						send_line = "<space>sl",
+						send_paragraph = "<space>sp",
+						send_until_cursor = "<space>su",
+						send_mark = "<space>sm",
+						mark_motion = "<space>mc",
+						mark_visual = "<space>mc",
+						remove_mark = "<space>md",
+						cr = "<space>s<cr>",
+						interrupt = "<space>s<space>",
+						exit = "<space>sq",
+						clear = "<space>cl",
+					},
+					-- If the highlight is on, you can change how it looks
+					-- For the available options, check nvim_set_hl
+					highlight = {
+						italic = true
+					},
+					ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+
+				}
+			)
+
+			Utils.set_custom_maps(
+				{
+					[""] =
+					{
+						{ "n", "<leader>rs", "<cmd>IronRepl<cr>",    { desc = "Toggle Iron REPL" } },
+						{ "n", "<leader>rr", "<cmd>IronRestart<cr>", { desc = "Restart Iron REPL" } },
+						{ "n", "<leader>rf", "<cmd>IronFocus<cr>",   { desc = "Focus Iron REPL" } },
+						{ "n", "<leader>rh", "<cmd>IronHide<cr>",    { desc = "Hide Iron REPL" } },
+					}
+				}
+			)
+		end
+	},
 }
