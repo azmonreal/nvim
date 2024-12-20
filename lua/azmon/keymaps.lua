dd363004f7827179142bfdf2f36328f726d85ff6
azmonreal <azmonreal@gmail.com>
2024-12-20 11:14:40 -0600

    feat: move lines up/down keymaps

--- lua/azmon/keymaps.lua
+++ lua/azmon/keymaps.lua
@@ -1,9 +1,14 @@
 Utils.setKeymaps({
 	{
-		{ "]b", "<cmd>bn<CR>", { desc = "Switch to next buffer" } },
-		{ "[b", "<cmd>bp<CR>", { desc = "Switch to previous buffer" } },
-		{ "]q", "<cmd>cn<CR>", { desc = "Select next quickfix item" } },
-		{ "[q", "<cmd>cp<CR>", { desc = "Select previous quickfix item" } },
+		{ "]b",    "<cmd>bn<CR>",      { desc = "Switch to next buffer" } },
+		{ "[b",    "<cmd>bp<CR>",      { desc = "Switch to previous buffer" } },
+		{ "]q",    "<cmd>cn<CR>",      { desc = "Select next quickfix item" } },
+		{ "[q",    "<cmd>cp<CR>",      { desc = "Select previous quickfix item" } },
+
+		{ "<M-J>", "<cmd>m +1<CR>==",  { desc = "Move line one below" } },
+		{ "<M-K>", "<cmd>m -2<CR>==",  { desc = "Move line one above" } },
+		{ "<M-J>", ":m '>+<CR>gv=gv",  { desc = "Move selection one below" },       mode = "v" },
+		{ "<M-K>", ":m '<-2<CR>gv=gv", { desc = "Move selection one above" },       mode = "v" },
 	},
 
 	["Z"] = {
@@ -11,5 +16,5 @@ Utils.setKeymaps({
 		{ "Z", function () vim.cmd([[wa|qa]]) end, { desc = "Save all buffers and quit" } },
 		{ "q", function () vim.cmd([[bd!]]) end,   { desc = "Delete current buffer without saving" } },
 		{ "Q", function () vim.cmd([[qa!]]) end,   { desc = "Exit all buffers without saving" } },
-	}
+	},
 })
