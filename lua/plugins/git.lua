178e544161d31a0b2a17bc5fd11746c1347bd339
azmonreal <azmonreal@gmail.com>
2026-03-31 10:39:28 -0600

    feat: gitgraph

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -82,4 +82,35 @@ return {
 	{
 		"sindrets/diffview.nvim",
 	},
+	{
+		"isakbm/gitgraph.nvim",
+		opts = {
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function (commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function (from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gL",
+				function ()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
+		},
+	},
+
 }
