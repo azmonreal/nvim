f6599d5b6437505f44039dea83a7fc25e0a0f96f
azmonreal <azmonreal@gmail.com>
2026-03-31 10:28:39 -0600

    feat: comprehensive gitsings (hunk related) keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,22 +12,46 @@ return {
 			},
 
 			on_attach = function (bufnr)
-				local gs = package.loaded.gitsigns
-
-				-- Navigation
+				local gitsigns = require("gitsigns")
 				Utils.setKeymaps({
 					{
+						-- Navigation
 						{ "]c", function ()
-							if vim.wo.diff then return "]c" end
-							vim.schedule(function () gs.next_hunk() end)
-							return "<Ignore>"
-						end, { expr = true } },
+							if vim.wo.diff then
+								vim.cmd.normal({ "]c", bang = true })
+							else
+								gitsigns.nav_hunk("next")
+							end
+						end },
 						{ "[c", function ()
-							if vim.wo.diff then return "[c" end
-							vim.schedule(function () gs.prev_hunk() end)
-							return "<Ignore>"
-						end, { expr = true } },
-						{ "<leader>gB", function () gs.blame_line { full = true } end, { desc = "Git blame" } },
+							if vim.wo.diff then
+								vim.cmd.normal({ "[c", bang = true })
+							else
+								gitsigns.nav_hunk("prev")
+							end
+						end },
+						-- Toggles
+						{ "<leader>gtb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" } },
+						{ "<leader>gtw", gitsigns.toggle_word_diff,          { desc = "Toggle word diff" } },
+						-- Text object
+						{ "ih",          gitsigns.select_hunk,               { desc = "Select hunk" },              mode = { "o", "x" } },
+					},
+					["<leader>h"] = {
+						-- Actions
+						{ "s", gitsigns.stage_hunk,                                                         { desc = "Stage hunk" } },
+						{ "r", gitsigns.reset_hunk,                                                         { desc = "Reset hunk" } },
+						{ "s", function () gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "Stage hunk (visual)" }, mode = "v" },
+						{ "r", function () gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "Reset hunk (visual)" }, mode = "v" },
+						{ "S", gitsigns.stage_buffer,                                                       { desc = "Stage buffer" } },
+						{ "R", gitsigns.reset_buffer,                                                       { desc = "Reset buffer" } },
+						{ "p", gitsigns.preview_hunk,                                                       { desc = "Preview hunk" } },
+						{ "i", gitsigns.preview_hunk_inline,                                                { desc = "Preview hunk inline" } },
+						{ "b", function () gitsigns.blame_line({ full = true }) end,                        { desc = "Blame line" } },
+						{ "d", gitsigns.diffthis,                                                           { desc = "Diff this" } },
+						{ "D", function () gitsigns.diffthis("~") end,                                      { desc = "Diff this ~" } },
+						{ "Q", function () gitsigns.setqflist("all") end,                                   { desc = "Set qflist (all)" } },
+						{ "q", gitsigns.setqflist, { desc = "Set qflist" },
+						},
 					},
 				})
 			end,
