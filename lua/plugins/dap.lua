b6e1d4f698e484ce27e781f1e59556ead927e424
azmonreal <azmonreal@gmail.com>
2026-04-03 09:50:22 -0600

    wip: rustaceanvim (with dap)

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -14,39 +14,50 @@ return {
 		"nvim-neotest/nvim-nio",
 	},
 	lazy = true,
+	init = function ()
+		Utils.setKeymaps(
+			{
+				{
+					{ "<F8>",  "<cmd>DapContinue<CR>" },
+					{ "<F9>",  "<cmd>DapToggleBreakpoint<CR>" },
+					{ "<F10>", "<cmd>DapStepOver<CR>" },
+					{ "<F11>", "<cmd>DapStepInto<CR>" },
+					{ "<F12>", "<cmd>DapStepOut<CR>" },
+				},
+			}
+		)
+	end,
 	config = function ()
 		local dap = require("dap")
 
-		dap.adapters.lldb = {
+		dap.adapters.cppdbg = {
+			id = "cppdbg",
 			type = "executable",
-			command = "/usr/bin/lldb-vscode", -- adjust as needed, must be absolute path
-			name = "lldb",
+			command = "OpenDebugAD7", -- adjust as needed, must be absolute path
 		}
 
 		dap.configurations.cpp = {
 			{
-				name = "Launch",
-				type = "lldb",
+				name = "Launch file",
+				type = "cppdbg",
 				request = "launch",
 				program = function ()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
-				stopOnEntry = true,
-				args = {},
-
-				-- 💀
-				-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-				--
-				--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-				--
-				-- Otherwise you might get the following error:
-				--
-				--    Error on launch: Failed to attach to the target process
-				--
-				-- But you should be aware of the implications:
-				-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-				-- runInTerminal = false,
+				stopAtEntry = true,
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "cppdbg",
+				request = "launch",
+				MIMode = "gdb",
+				miDebuggerServerAddress = "localhost:1234",
+				miDebuggerPath = "/usr/bin/gdb",
+				cwd = "${workspaceFolder}",
+				program = function ()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
 			},
 		}
 	end,
