b6248e7674d62a655d6d75e74f3ece559ca378f4
azmonreal <azmonreal@gmail.com>
2026-04-22 01:01:40 -0600

    feat: tsls inlay hints

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -1,7 +1,26 @@
+local ts_inlayHints = {
+	includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+	includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+	includeInlayVariableTypeHints = true,
+	includeInlayFunctionParameterTypeHints = true,
+	includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+	includeInlayPropertyDeclarationTypeHints = true,
+	includeInlayFunctionLikeReturnTypeHints = true,
+	includeInlayEnumMemberValueHints = true,
+}
+
 return {
 	init_options = {
 		preferences = {
 			importModuleSpecifierPreference = "non-relative",
+		},
+	},
+	settings = {
+		typescript = {
+			inlayHints = ts_inlayHints,
+		},
+		javascript = {
+			inlayHints = ts_inlayHints,
 		},
 	},
 }
